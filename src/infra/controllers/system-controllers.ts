59587d9bc9664248b13c0d0cdcdfc3630b57adf2
dnanseldev <dnansel.dev@gmail.com>
2022-10-04 07:43:39 -0300

    out 4 07:43 controller refactoring

--- src/infra/controllers/system-controllers.ts
+++ src/infra/controllers/system-controllers.ts
@@ -5,8 +5,13 @@ class SystemController
 {
     public async register(req: Request, res: Response): Promise<Response>
     {
-        const s_dto: SystemDTO
-         {name, description, domain_url} = req.body
-         return res.json(s_dto)
+        const s_dto: SystemDTO = {
+             sid: '123456'
+            ,name: req.body.name
+            ,description: req.body.description
+            ,domain_url: req.body.domain_url
+        }        
+
+        return res.json(s_dto)
     }
 }
